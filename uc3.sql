d88b53cc9aba5754cfe71349a160994af4f1f5e4
ANURAG-prakash <78632929+ANURAG-prakash@users.noreply.github.com>
2021-04-12 07:55:25 +0530

    Added

---
+++ uc3.sql
@@ -0,0 +1,16 @@
+create database AddressBookService
+use AddressBookService
+create table AddressBookTable1
+(
+firstname varchar(50) not null,
+lastname varchar(50) not null,
+address varchar(50) not null,
+city varchar(50) not null,
+state varchar(50) not null,
+zip varchar(50) not null,
+phonenumber varchar(50) not null,
+email varchar(50) not null
+);
+select * from AddressBookTable1
+insert into AddressBookTable1 values
+('Anurag','Prakash','mango','jhamshedpur','jharkhand',831012,'9518039211','Prakash.Shanu80@gmail.com');
